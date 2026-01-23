8e423de800320216d8e2f5669ddb0571778e40c6
Shrey Shukla <shreys576@gmail.com>
2026-01-23 16:09:02 +0530

    Refactor main function to allow dynamic input for array elements and key

--- DSA/linearsearch.cpp
+++ DSA/linearsearch.cpp
@@ -17,8 +17,23 @@ int linearsearch(vector<int> arr, int key){
 
 int main(){
 
-    vector<int> arr = {3,1,6,4,2};
-    int key = 5;
+    int n;
+    int key;
+    vector<int> arr;
+    cout << "How many elements do you want in your array: ";
+    cin >> n;
+    cout << endl;
+    for(int i = 0; i<n; i++){
+        cout << "Enter element: ";
+        int temp;
+        cin >> temp;
+        arr.push_back(temp);
+        cout << endl;
+    }
+    cout << endl;
+    cout << "Enter Key: ";
+    cin >> key;
+    cout << endl;
     cout << linearsearch(arr, key);
 
 }
